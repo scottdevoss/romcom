54530302bbd6827c18643be9efa9529a606cd900
Scott DeVoss <scottdevoss@yahoo.com>
2024-05-22 09:51:21 -0600

    completed iteration 0

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 // Create variables targetting the relevant DOM elements here 👇
-
+var randomBtn = document.querySelector('.random-cover-button');
+var homeImg = document.querySelector('.cover-image');
+var homeTitle = document.querySelector('.cover-title');
+var homeDesc1 = document.querySelector('.tagline-1');
+var homeDesc2 = document.querySelector('.tagline-2');
+//var currentCover = createRandomCover();
 
 // We've provided a few variables below
 var savedCovers = [
   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
 
 // Add your event listeners here 👇
-
+randomBtn.addEventListener('click', createRandomCover);
 
 // Create your event handlers and other functions here 👇
 
@@ -27,4 +31,15 @@ function createCover(imgSrc, title, descriptor1, descriptor2) {
     tagline2: descriptor2
   }
   return cover
+}
+
+function createRandomCover() {
+  var randomCover = covers[getRandomIndex(covers)];
+  var randomTitle = titles[getRandomIndex(titles)];
+  var randomDescriptor1 = descriptors[getRandomIndex(descriptors)];
+  var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
+  homeImg.src = randomCover;
+  homeTitle.innerText = randomTitle;
+  homeDesc1.innerText = randomDescriptor1;
+  homeDesc2.innerText = randomDescriptor2;
 }
